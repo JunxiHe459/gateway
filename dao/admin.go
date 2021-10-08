bcf1b11fbc2faeb8b2f9e3d381860ebd08eb6cfd
Junxi He <junxihe459@gmail.com>
2021-10-09 07:07:02 +0800

    修复了数据库 time.time bug

--- dao/admin.go
+++ dao/admin.go
@@ -6,16 +6,19 @@ import (
 	"github.com/JunxiHe459/gateway/public"
 	"github.com/e421083458/gorm"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 type Admin struct {
-	Id        int    `json:"id" gorm:"primary_key" description:"自增主键"`
-	UserName  string `json:"user_name" gorm:"column:user_name" description:"管理员用户名"`
-	Salt      string `json:"salt" gorm:"column:salt" description:"盐"`
-	Password  string `json:"password" gorm:"column:password" description:"密码"`
-	UpdatedAt string `json:"update_at" gorm:"column:update_at" description:"更新时间"`
-	CreatedAt string `json:"create_at" gorm:"column:create_at" description:"创建时间"`
-	IsDelete  int    `json:"is_delete" gorm:"column:is_delete" description:"是否删除"`
+	Id       int    `json:"id" gorm:"primary_key" description:"自增主键"`
+	UserName string `json:"user_name" gorm:"column:user_name" description:"管理员用户名"`
+	Salt     string `json:"salt" gorm:"column:salt" description:"盐"`
+	Password string `json:"password" gorm:"column:password" description:"密码"`
+	//UpdatedAt string `json:"update_at" gorm:"column:update_at" description:"更新时间"`
+	//CreatedAt string `json:"create_at" gorm:"column:create_at" description:"创建时间"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	IsDelete  int `json:"is_delete" gorm:"column:is_delete" description:"是否删除"`
 }
 
 func (admin *Admin) TableName() string {
